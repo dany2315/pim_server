d7c6ed12d71887ee8e44fcc7de4629665379a7be
Dany serfaty <davidserfaty2315@gmail.com>
2023-07-20 23:21:03 +0300

    manque jwt

--- controller/authController.js
+++ controller/authController.js
@@ -1,19 +1,22 @@
 import User from '../models/modelUser.js'
 import bcrypt from 'bcrypt'
 
+
 export const login = async(req,res) =>{
     const {identifiant,password} = req.body
 try {
     const user = await User.findOne({identifiant})
     if (!user) {
-        res.status(401).send({msg:"identifiant incorrect",status:"error"})
+        throw new Error("identifiant incorrect");
+       // res.send({msg:"identifiant incorrect",status:"error"})
     }
     const ismatche = await user.comparePassword(password)
     if (!ismatche) {
-        res.status(401).send({msg:"mot de passe incorrect",status:"error"})
+        throw new Error("mot de passe incorrect");
+        //res.send({msg:"mot de passe incorrect",status:"error"})
     }
-    res.status(200).send({msg:"Autentification reussi",status:"success"})
+    res.status(200).send({message:"Autentification reussi"})
 } catch (error) {
-    res.status(500).send({msg:"erreur de connexion",status:"error"})
+    res.status(401).send({message:error.message})
 }
 } 
